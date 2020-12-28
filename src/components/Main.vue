c9478339ab144fb709a219ac3de17bc2e7d11280
unknown <tomerpacific@gmail.com>
2020-12-28 22:23:07 +0200

    ProjectChecker created logic to fetch all urls in array

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -35,21 +35,29 @@ export default {
   },
   created() {
     const that = this;
-    fetch(`${BASE_URL}websiteStatus?url=https://tomerpacific.github.io/firebaseScraper/`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (Object.prototype.hasOwnProperty.call(data, 'website')
+    Promise.all(projectEndpoints.map((endpoint) => new Promise((resolve, reject) => {
+      fetch(`${BASE_URL}websiteStatus?url=${endpoint}`)
+        .then((response) => response.json())
+        .then((data) => {
+          if (Object.prototype.hasOwnProperty.call(data, 'website')
       && Object.prototype.hasOwnProperty.call(data, 'websiteStatus')) {
-          that.statuses.push({
-            name: data.website,
-            status: data.websiteStatus,
-          });
-        } else {
-          that.statuses.push({
-            name: 'Error',
-            status: 'No information was received from the server',
-          });
-        }
+            that.statuses.push({
+              name: data.website,
+              status: data.websiteStatus,
+            });
+            resolve();
+          } else {
+            that.statuses.push({
+              name: 'Error',
+              status: 'No information was received from the server',
+            });
+            resolve();
+          }
+          reject();
+        });
+    })))
+      .then((results) => {
+        console.log(results);
       })
       .catch((error) => {
         that.statuses.push({
@@ -57,6 +65,22 @@ export default {
           status: error,
         });
       });
+    // fetch(`${BASE_URL}websiteStatus?url=https://tomerpacific.github.io/firebaseScraper/`)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     if (Object.prototype.hasOwnProperty.call(data, 'website')
+    //   && Object.prototype.hasOwnProperty.call(data, 'websiteStatus')) {
+    //       that.statuses.push({
+    //         name: data.website,
+    //         status: data.websiteStatus,
+    //       });
+    //     } else {
+    //       that.statuses.push({
+    //         name: 'Error',
+    //         status: 'No information was received from the server',
+    //       });
+    //     }
+    //   })
   },
 };
 </script>
