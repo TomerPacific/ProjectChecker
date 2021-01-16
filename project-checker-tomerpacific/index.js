2f652706de5ac94ec42cb953d827c927197ba760
unknown <tomerpacific@gmail.com>
2021-01-16 18:26:22 +0200

    ProjectChecker BED using nodemailer to send emails if any service is down

--- project-checker-tomerpacific/index.js
+++ project-checker-tomerpacific/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 var port = process.env.PORT || 3000
+require('dotenv').config()
 
 const fetch = require("node-fetch");
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -24,6 +28,9 @@ app.use(bodyParser.json());
           { website: url,
             websiteStatus: response.status
           })
+          //if (response.status !== 200) {
+              sendMail(url, response.status);
+         // }
     })
     .catch(function(error) {
       res.status(500).send(
@@ -32,6 +39,36 @@ app.use(bodyParser.json());
         })
     })
   })
+
+  function sendMail(website, status) {
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+      }
+    });
+
+    let mailOptions = {
+      from: process.env.MAIL_USERNAME,
+      to: process.env.MAIL_USERNAME,
+      subject: 'ProjectChecker - Website Is Down',
+      text: 'The website ' + website + ' returned with a status of ' + status
+    };
+
+    transporter.sendMail(mailOptions, function(err, data) {
+      if (err) {
+        console.log("Error " + err);
+      } else {
+        
+      }
+    });
+
+  }
 
   app.listen(port, 
     () => console.log(`Example app listening at http://localhost:${port}`))
