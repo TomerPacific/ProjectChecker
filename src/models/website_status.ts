06b7431f4dd92316be1829f7802bdaeda7198157
unknown <tomerpacific@gmail.com>
2022-09-08 21:27:16 +0300

    ProjectChecker removing project

--- src/models/website_status.ts
+++
@@ -1,4 +0,0 @@
-export type WebsiteStatus = {
-    url: string,
-    status: number
-}
--- src/models/website_status_response.ts
+++
@@ -1,5 +0,0 @@
-import { WebsiteStatus } from "./website_status";
-
-export type WebsiteStatusResponse = {
-    websites: Array<WebsiteStatus>
-}
