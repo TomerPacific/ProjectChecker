8c8843599f3de49a1f9ad36aeb658bfe4a7347cb
unknown <tomerpacific@gmail.com>
2022-09-09 14:43:23 +0300

    ProjectChecker adding constants class

---
+++ project-checker/src/constants.ts
@@ -0,0 +1,14 @@
+const projectEndpoints = [
+    'https://tomerpacific.github.io/firebaseScraper/',
+    'https://tomerpacific.github.io/julOnSale/',
+    'https://tomerpacific.github.io/Android-XML-Menu-Generator/',
+    'https://tomerpacific.github.io/MediumArticleFetcher/',
+    'https://tomerpacific.github.io/pull-request-presenter/',
+    'https://tomerpacific.github.io/Portfolio/',
+    'https://tomerpacific.github.io/resume-reviewer/',
+    'https://tomerpacific.github.io/github-utils/',
+  ];
+
+  module.exports = {
+      projectEndpoints: projectEndpoints
+  }
