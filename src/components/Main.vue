e36bd6f7912766b8a0df9c95c300fbd736637f75
unknown <tomerpacific@gmail.com>
2024-11-30 20:05:56 +0200

    ProjectChecker adding loader

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,61 +1,62 @@
 <script setup lang="ts">
-  import { ref, onMounted } from 'vue'
-  import { WebsiteStatus } from '../models/website_status';
-  import { WebsiteStatusResponse } from '../models/website_status_response';
-  const BASE_URL = 'https://project-checker.onrender.com/checkStatus';
-  defineProps(['msg'])
-  let isLoaded = ref(false)
-  let statuses = ref([])
+import { ref, onMounted } from 'vue'
+import { WebsiteStatus } from '../models/website_status'
+import { WebsiteStatusResponse } from '../models/website_status_response'
+const BASE_URL = 'https://project-checker.onrender.com/checkStatus'
+defineProps(['msg'])
+const isLoaded = ref(false)
+const statuses = ref([])
 
-  function extractServiceNameFromUrl(endpoint: string): string {
-    const splitEndpoint = endpoint.split('/');
-    const endpointName = splitEndpoint[splitEndpoint.length - 2];
-    return endpointName;
-  }
+function extractServiceNameFromUrl(endpoint: string): string {
+  const splitEndpoint = endpoint.split('/')
+  const endpointName = splitEndpoint[splitEndpoint.length - 2]
+  return endpointName
+}
 
-  function getWebsitesStatus() {
-    return new Promise<Array<WebsiteStatus>>((resolve, reject) => {
-        fetch(BASE_URL)
-          .then((result) => result.json())
-          .then((data: WebsiteStatusResponse) => {
-            resolve(data.websites);
-          })
-          .catch((error) => {
-            statuses.value.push({
-              url: 'Error',
-              status: error,
-            });
-            reject();
-          });
-      });
-  }
-
-  onMounted(() => {
-    getWebsitesStatus()
-    .then((websites: Array<WebsiteStatus>) => {
-        statuses.value = websites;
-        isLoaded.value = true;
+function getWebsitesStatus() {
+  return new Promise<Array<WebsiteStatus>>((resolve, reject) => {
+    fetch(BASE_URL)
+      .then((result) => result.json())
+      .then((data: WebsiteStatusResponse) => {
+        resolve(data.websites)
       })
-      .catch(() => {
-        isLoaded.value = false;
-      });
-  });
+      .catch((error) => {
+        statuses.value.push({
+          url: 'Error',
+          status: error,
+        })
+        reject()
+      })
+  })
+}
+
+onMounted(() => {
+  getWebsitesStatus()
+    .then((websites: Array<WebsiteStatus>) => {
+      statuses.value = websites
+      isLoaded.value = true
+    })
+    .catch(() => {
+      isLoaded.value = false
+    })
+})
 </script>
 
 <template>
   <div class="hello">
-    <h1>{{ msg }} <img id="logo" src="../assets/logo.png" /> </h1>
-    <h2 v-if="!isLoaded">Loading...</h2>
+    <h1>{{ msg }} <img id="logo" src="../assets/logo.png" /></h1>
+    <div v-if="!isLoaded" class="loader"></div>
     <ul v-if="isLoaded" class="projectsList">
       <li v-for="website in statuses" :key="website.name">
-        <a v-bind:href="website.name" target="_blank">{{extractServiceNameFromUrl(website.name)}}</a>
+        <a v-bind:href="website.name" target="_blank">{{
+          extractServiceNameFromUrl(website.name)
+        }}</a>
         <span class="websiteStatus" v-if="website.status === 200">&#9989;</span>
         <span class="websiteStatus" v-else>&#10060;</span>
       </li>
     </ul>
   </div>
 </template>
-
 
 <style scoped>
 h3 {
@@ -85,5 +86,24 @@ a:hover {
 
 .websiteStatus {
   margin-left: 1%;
+}
+
+.loader {
+  margin: auto;
+  border: 16px solid #f3f3f3;
+  border-top: 16px solid #3498db;
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
