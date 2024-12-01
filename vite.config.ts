5f3be89b5e46dce3a8804b4ca60c25128c4303bf
unknown <tomerpacific@gmail.com>
2024-12-01 20:35:50 +0200

    ProjectChecker updating base attribute

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/ProjectChecker/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
