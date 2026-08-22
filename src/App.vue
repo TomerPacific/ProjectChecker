c84ab3fdef9d7bc2582215a24a9b63b8d347ecd6
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-08-22 10:34:47 +0300

    ProjectChecker updating main file

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,5 @@ import Main from './components/Main.vue'
 </script>
 
 <template>
-  <div id="app">
-    <Main msg="Welcome to Your ProjectChecker" />
-  </div>
+  <Main />
 </template>
-
-<style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
